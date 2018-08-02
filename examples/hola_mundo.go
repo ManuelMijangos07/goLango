3a3830970bd5b363523ccff5666a361a399405cd
Manuel Mijangos <mmijangos.c@outlook.com>
2018-08-02 08:48:21 -0600

    first class: variables, sleep, hi world

---
+++ examples/hola_mundo.go
@@ -0,0 +1,18 @@
+package main
+
+// import "time"
+
+import (
+	"fmt"
+	//"time"
+)
+
+func main() {
+	//time.Sleep(time.Second * 5) // retardo al ejecutar el programa
+	var suma int = 8 + 9
+	var resta int = 6 - 4
+
+	fmt.Println(suma)
+	fmt.Println(resta)
+	fmt.Println("Hola mundo")
+}
